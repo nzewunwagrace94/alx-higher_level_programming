c041f364998f06be665d6bac09be2f53b1067a35
nzewunwagrace94 <nzewunwagrace94>
2024-01-05 23:03:23 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     x = len(argv) - 1
-    if x < 1 :
+    if x < 1:
         print("{} arguments.".format(x))
     elif x == 1:
         print("{} argument:".format(x))
