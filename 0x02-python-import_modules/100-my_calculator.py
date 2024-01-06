a19ac95af710866b9291c7b2b08f16324dd8a316
nzewunwagrace94 <nzewunwagrace94>
2024-01-06 01:30:16 +0100

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,17 +6,17 @@ if __name__ == "__main__":
     if num_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-        a = int(argv[1])
-        operator = argv[2]
-        b = int(argv[3])
-        if operator == '+':
-            print(f"{a} {operator} {b} = {add(a, b)}")
-        elif operator == '-':
-            print(f"{a} {operator} {b} = {sub(a, b)}")
-        elif operator == '*':
-            print(f"{a} {operator} {b} = {mul(a, b)}")
-        elif operator == '/':
-            print(f"{a} {operator} {b} = {div(a, b)}")
-        else:
-            print("Unknown operator. available operators: +, -, * and /")
-            exit(1)
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+    if operator == '+':
+        print(f"{a} {operator} {b} = {add(a, b)}")
+    elif operator == '-':
+        print(f"{a} {operator} {b} = {sub(a, b)}")
+    elif operator == '*':
+        print(f"{a} {operator} {b} = {mul(a, b)}")
+    elif operator == '/':
+        print(f"{a} {operator} {b} = {div(a, b)}")
+    else:
+        print("Unknown operator. available operators: +, -, * and /")
+        exit(1)
