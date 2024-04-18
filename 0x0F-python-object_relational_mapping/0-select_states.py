496ca293c6a56c5448d48be4018202d48aafe1f5
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-04-18 09:46:24 +0100

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa sorted in ascending
-order by states id
-"""
+'''
+Get all states
+'''
+
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    def list_states(username, password, database):
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306, user=username,
-                passwd=password,
-                db=database)
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        results = cur.fetchall()
-        for row in results:
-            print(row)
-        db.close()
-
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    list_states(username, password, database)
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
