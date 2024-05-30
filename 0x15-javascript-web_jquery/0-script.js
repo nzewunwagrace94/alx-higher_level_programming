2fc4d4d6a54dfedb73d8147b91f6a35e85c1e498
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-05-30 09:24:54 +0100

    No JQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+/**
+ * JavaScript script that updates the text color of the
+ * <header> element to red (#FF0000):
+ */
+
+const header = document.querySelector('header');
+
+header.style.color = '#FF0000';
